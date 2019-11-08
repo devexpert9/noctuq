e00aae171c37642e2d1525cfbd44468ceee040a4
vishalrr <vishalindiit@gmail.com>
2019-11-08 18:08:27 +0530

    design changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login-host',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
